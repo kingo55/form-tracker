9cb100928ffd84f64764773c80efd55e740103b2
MartijnVisser <martijn@2symbols.com>
2012-12-07 15:19:20 +0100

    Update form-tracker.js
    Using .closest, it automatically retrieves the form ID.

--- form-tracker.js
+++ form-tracker.js
@@ -2,10 +2,10 @@
 $(document).ready(function() { 
 	$(':input').blur(function () {
 		if($(this).val().length > 0) {
-			_gaq.push(['_trackEvent', 'INSERT FORM NAME HERE', 'completed', $(this).attr('name')]);
+			_gaq.push(['_trackEvent', $(this).closest('form').attr('id'), 'completed', $(this).attr('name')]);
 		} 
 		else {
-			_gaq.push(['_trackEvent', 'INSERT FORM NAME HERE', 'skipped', $(this).attr('name')]);
+			_gaq.push(['_trackEvent', $(this).closest('form').attr('id'), 'skipped', $(this).attr('name')]);
 		}
 	});
 });
