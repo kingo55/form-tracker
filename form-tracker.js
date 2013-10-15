5f76db5e82993cc50e83c66893308d237cb48133
Robert Kingston <kingo55@gmail.com>
2013-10-15 19:56:23 +1100

    Updating it to push to Google Tag Manager's datalayer

--- form-tracker.js
+++ form-tracker.js
@@ -2,10 +2,20 @@
 $(document).ready(function() { 
 	$(':input').blur(function () {
 		if($(this).val().length > 0) {
-			_gaq.push(['_trackEvent', $(this).closest('form').attr('id'), 'completed', $(this).attr('name')]);
+			dataLayer.push([
+				'event': '_trackEvent',
+				'eventCategory': $(this).closest('form').attr('id'),
+				'eventAction': 'completed',
+				'eventLabel': $(this).attr('name')
+			]);
 		} 
 		else {
-			_gaq.push(['_trackEvent', $(this).closest('form').attr('id'), 'skipped', $(this).attr('name')]);
+			dataLayer.push([
+				'event': '_trackEvent',
+				'eventCategory': $(this).closest('form').attr('id'),
+				'eventAction': 'skipped',
+				'eventLabel': $(this).attr('name')
+			]);
 		}
 	});
 });
